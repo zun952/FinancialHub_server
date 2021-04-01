f6a3636091473953f83111c6c75bca7910368df9
DM <gofire99@naver.com>
2021-04-01 21:51:24 +0900

    remove server folder

--- server/build/Index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Index = /** @class */ (function () {
-    function Index() {
-    }
-    return Index;
-}());
-exports.default = Index;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiSW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvSW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQTtJQUFBO0lBRUEsQ0FBQztJQUFELFlBQUM7QUFBRCxDQUFDLEFBRkQsSUFFQztBQUVELGtCQUFlLEtBQUssQ0FBQyJ9
